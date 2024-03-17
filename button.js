3892f934f10c8aa9ebf959674328ca8d94d6d4b9
vidya7483 <veenarasalakar131@gmail.com>
2024-03-17 22:47:13 +0530

    background color change

---
+++ button.js
@@ -0,0 +1,13 @@
+const button=document.querySelector('button')
+const body=document.querySelector('body')
+const color=['blue','black','green','aqua','brown','gray','violet']
+
+body.style.backgroundColor= 'violet'
+
+button.addEventListener('click',changeB)
+
+function changeB()
+{
+    const colorIndex=parseInt(Math.random()*color.length)
+    body.style.backgroundColor=color[colorIndex]
+} 
